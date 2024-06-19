aa61972f66ce5f546186168dde9322c03a0e3997
Petyo Stoyanov <petyo.stoyanov@inabit.eu>
2024-06-19 11:43:22 +0300

    Fix timer

--- core/app_delete_data_logic.go
+++ core/app_delete_data_logic.go
@@ -60,7 +60,7 @@ func (d deleteDataLogic) setupTimerForDelete() {
 	}
 
 	//wait until it is the correct moment from the day
-	/*location, err := time.LoadLocation("America/Chicago")
+	location, err := time.LoadLocation("America/Chicago")
 	if err != nil {
 		d.logger.Errorf("Error getting location:%s\n", err.Error())
 	}
@@ -80,9 +80,9 @@ func (d deleteDataLogic) setupTimerForDelete() {
 		leftToday := 86400 - nowSecondsInDay
 		durationInSeconds = leftToday + desiredMoment // the time which left today + desired moment from tomorrow
 	}
-	log.Println(durationInSeconds) */
-	duration := time.Second * time.Duration(3)
-	//duration := time.Second * time.Duration(durationInSeconds)
+	//log.Println(durationInSeconds)
+	//duration := time.Second * time.Duration(3)
+	duration := time.Second * time.Duration(durationInSeconds)
 	d.logger.Infof("setupTimerForDelete -> first call after %s", duration)
 
 	d.dailyDeleteTimer = time.NewTimer(duration)
