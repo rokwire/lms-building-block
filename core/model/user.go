dda5e42cc03571c56e2ec9d393f028553820b48f
Stefan Vitanov <stefan@inabyte.com>
2025-01-27 13:28:10 +0200

    fix the model and the docs

--- core/model/user.go
+++ core/model/user.go
@@ -33,48 +33,10 @@ type DeletedMembership struct {
 
 // UserDataResponse represents a user data response
 type UserDataResponse struct {
-	SentNudgeResponse     []SentNudgeResponse       `json:"sent_nudges"`
-	NudgesProcessResponse []NudgesProcessesResponse `json:"nudges_processes"`
-	NudgesBlocksResponse  []NudgesBlocksResponse    `json:"nudges_blocks"`
-	UserContentResponse   []UserContentResponse     `json:"user_contents"`
-	UserCoursesResponse   []UserCoursesResponse     `json:"user_courses"`
-	UserUnitsResponse     []UserUnitsResponse       `json:"user_units"`
-}
-
-// SentNudgeResponse entity
-type SentNudgeResponse struct {
-	ID      string `json:"id"`
-	UserID  string `json:"user_id"`
-	NudgeID string `json:"nudge_id"`
-}
-
-// NudgesProcessesResponse entity
-type NudgesProcessesResponse struct {
-	ID     string `json:"id"`
-	UserID string `json:"user_id"`
-	Status string `json:"status"`
-}
-
-// NudgesBlocksResponse entity
-type NudgesBlocksResponse struct {
-	ID     string `json:"process_id"`
-	UserID string `json:"user_id"`
-}
-
-// UserContentResponse entity
-type UserContentResponse struct {
-	ID     string `json:"id" bson:"_id"`
-	UserID string `json:"user_id"`
-}
-
-// UserCoursesResponse entity
-type UserCoursesResponse struct {
-	ID     string `json:"id" bson:"_id"`
-	UserID string `json:"user_id"`
-}
-
-// UserUnitsResponse entity
-type UserUnitsResponse struct {
-	ID     string `json:"id" bson:"_id"`
-	UserID string `json:"user_id"`
+	ProviderCourses    []ProviderCourse   `json:"my_provider_courses"`
+	ProviderAssignment []CourseAssignment `json:"my_provider_assignments"`
+	ProviderAccount    ProviderUser       `json:"provider_account"`
+	Courses            []UserCourse       `json:"my_courses"`
+	Units              []UserUnit         `json:"my_unit"`
+	Content            []UserContent      `json:"my_contents"`
 }
